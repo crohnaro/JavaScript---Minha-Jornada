893170cc090dd7ab5a14d1e49e9ed614d81f30d4
crohnaro <viniciosalfa@hotmail.com>
2022-03-12 17:14:50 -0300

    subindo mais exercicios de pratica js

---
+++ 08.js
@@ -0,0 +1,19 @@
+let botaoTeste = document.querySelector('#botaoTeste')
+
+let contato = 0
+
+function cliqueBotao(){
+    contador ++
+
+    console.log('mensagem 1')
+
+    if (contador >= 5){
+        botaoTeste.removeEventListener('click' , cliqueBotao)
+    }
+}
+
+// addEventListener
+botaoTeste.addEventListener('click' , cliqueBotao)
+
+//onclick
+//botaoTeste.onclick = cliqueBotao
