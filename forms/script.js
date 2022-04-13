f7f2fafd50c3701742108f9ed5f3f68f0da39a75
crohnaro <viniciosalfa@hotmail.com>
2022-04-13 14:52:56 -0300

    melhorando tratativa do forms usando js

--- forms/script.js
+++ forms/script.js
@@ -3,5 +3,14 @@ let formulario = document.querySelector('#formCadastro')
 formulario.onsubmit = function(evento){
     evento.preventDefault()
 
-    console.log(document.forms['formCadastro']['nome'].value)
+    let inputNome = document.forms['formCadastro']['nome']
+
+    if(!inputNome.value){
+        inputNome.classList.add('inputError')
+
+        let span = inputNome.nextSibling.nextSibling
+        span.innerText = 'Digite o nome corretamente'
+    }else{
+        
+    }
 }
