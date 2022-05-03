2ad4b63017d20531be12552c478aa401720877d8
crohnaro <viniciosalfa@hotmail.com>
2022-05-03 11:28:16 -0300

    Subindo pratica de arrow functions

---
+++ Arrow Functions/main.js
@@ -0,0 +1,32 @@
+/* 
+
+Arrow Functions
+
+ES6
+
+As funções de seta nos permitem escrever uma sintae de função mais curta
+
+*/
+
+const soma = (param1, param2) => {
+    return param1 + param2
+}
+
+//retorno implicito
+const teste = (param1, param2) => param1 + param2
+
+
+const resultado = teste(1, 2)
+console.log(resultado)
+
+
+//Quando tem 1 parametro pode remover paranteses
+const teste2 = param1 => param1
+
+console.log(teste2('ok'))
+
+const botao = document.querySelector('#botao')
+
+botao.onclick = () => {
+    console.log(this)
+}
